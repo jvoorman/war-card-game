cef8bcbb829532d202d1babc8239ffc2a0381721
Jenna Voorman <jennavoorman@gmail.com>
2021-03-08 15:16:44 -0500

    Added customizable input features

--- game.py
+++ game.py
@@ -94,10 +94,89 @@ class PilePile(object):
         except:
             self.add_pile(CardPile(self.deck_id, to_pile_name, cards_to_transfer))
 
+def war_function(player_list, pile_pile):
+    """Recursive function that decides the winner when War has to happen"""
+    # draw cards from all player_piles to compare
+    cards_to_compare = []
+    for player in player_list:
+        player_pile = pile_pile.get_pile(f'{player}_draw')
+        card = player_pile.draw_cards_from_top_of_pile(1)
+        cards_to_compare.append(card)
+    # compares all the players' cards
+    max_card_val = 0
+    ind_list = []
+    for i in range(len(cards_to_compare)):
+        card_str = cards_to_compare[i][:-1]
+        card_dict = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, '0': 10}
+        if card_str in card_dict:
+            card_val = card_dict[card_str]
+        else:
+            card_val = int(card_str)
+        if card_val > max_card_val:
+            max_card_val = card_val
+            ind_list = [i]
+        elif card_val == max_card_val:
+            ind_list.append(i)
+    max_value_player_list = [player_list[i] for i in ind_list]
+    # put cards into card_pot
+    try:
+        card_pot = pile_pile.get_pile('card_pot')
+        card_pot.add_to_pile(",".join(cards_to_compare))
+    except:
+        card_pot = CardPile(pile_pile.deck_id, 'card_pot', ",".join(cards_to_compare))
+        pile_pile.add_pile(card_pot)
+    for player in player_list:
+        player_pile = pile_pile.get_pile(f'{player}_draw')
+        card = player_pile.draw_cards_from_top_of_pile(1)
+        card_pot.add_to_pile(card)
+    # handle War scenario
+    if len(max_value_player_list) > 1:
+        if len(max_value_player_list) == 2:
+            winners = " and ".join(max_value_player_list)
+        else:
+            winners = ", ".join(max_value_player_list)
+        print(f"There's a {len(max_value_player_list)}-way tie between {winners}!")
+        print("This means War again!")
+
+        # clean up piles and remove appropriate players for War
+        players_to_remove = []
+        for player in max_value_player_list:
+            player_draw_pile = pile_pile.get_pile(f'{player}_draw')
+            try:
+                player_win_pile = pile_pile.get_pile(f'{player}_win')
+                win_pile_count = player_win_pile.remaining
+            except:
+                win_pile_count = 0
+            if win_pile_count + player_draw_pile.remaining < 2:
+                print(f"{player} does not have enough cards left! {player} is out of the game.")
+                # all player cards need to go into card_pot so make sure they're all in draw pile
+                if win_pile_count > 0:
+                    pile_pile.pile_to_pile_transfer(f'{player}_win', f'{player}_draw')
+                players_to_remove.append(player)
+            elif player_draw_pile.remaining < 2:
+                pile_pile.pile_to_pile_transfer(f'{player}_win', f'{player}_draw')
+        if len(players_to_remove) > 0:
+            if len(players_to_remove) == len(max_value_player_list):
+                print('Whoops, looks like War isn\'t gonna happen.')
+                print('Since it\'s way too much work to create a real solution, first player wins.')
+                players_to_remove = players_to_remove[1:]
+            for player in players_to_remove:
+                pile_pile.pile_to_pile_transfer(f'{player}_draw', 'card_pot')
+                max_value_player_list.remove(player)
+
+        if len(max_value_player_list) > 1:
+            winner = war_function(max_value_player_list, pile_pile)
+        else:
+            winner = max_value_player_list[0]
+    else:
+        winner = max_value_player_list[0]
+    # give cards to turn winner
+    pile_pile.pile_to_pile_transfer('card_pot', f'{winner}_win')
+    print(f"{winner} has the higher card!")
+    return winner
 
 def take_a_turn(player_list, pile_pile):
     """Takes in a Pilepile and player_list and performs all necessary functions to take a turn"""
-    print(f"Turn starting player list: {player_list}") 
     # draw cards from all player_piles to compare
     cards_to_compare = []
     for player in player_list:
@@ -140,7 +219,6 @@ def take_a_turn(player_list, pile_pile):
         players_to_remove = []
         for player in max_value_player_list:
             player_draw_pile = pile_pile.get_pile(f'{player}_draw')
-            print(f'Remaining for {player}: {player_draw_pile.remaining}')
             try:
                 player_win_pile = pile_pile.get_pile(f'{player}_win')
                 win_pile_count = player_win_pile.remaining
@@ -154,7 +232,6 @@ def take_a_turn(player_list, pile_pile):
                 players_to_remove.append(player)
             elif player_draw_pile.remaining < 2:
                 pile_pile.pile_to_pile_transfer(f'{player}_win', f'{player}_draw')
-                print(f"Cards transferred for {player}")
         if len(players_to_remove) > 0:
             if len(players_to_remove) == len(max_value_player_list):
                 print('Whoops, looks like War isn\'t gonna happen.')
@@ -165,22 +242,40 @@ def take_a_turn(player_list, pile_pile):
                 max_value_player_list.remove(player)
 
         if len(max_value_player_list) > 1:
-            take_a_turn(max_value_player_list, pile_pile)
+            winner = war_function(max_value_player_list, pile_pile)
+        else:
+            winner = max_value_player_list[0]
+    else:
+        winner = max_value_player_list[0]
     # give cards to turn winner
-    winner = max_value_player_list[0]
     pile_pile.pile_to_pile_transfer('card_pot', f'{winner}_win')
     print(f"{winner} won all of the cards!")
 
 def main():
     # initiate the game, dealing out all cards
     game_deck = Deck()
-    ## handle test cases for inputs
-    player_count = int(input('How many players would you like to play with? \n> '))
+    player_count = input('How many players would you like to play with? \n> ')
+    while True:
+        try:
+            assert(player_count.isnumeric())
+            assert(0 < int(player_count) < 52)
+            break
+        except:
+            player_count = input('I\'m sorry, please enter an appropriate number of players. \n> ')
     player_list = []
-    for i in range(player_count):
+    for i in range(int(player_count)):
         new_player = input(f'What is the name of player {i + 1}? \n> ')
+        while not new_player.replace(' ', '').isalnum():
+            new_player = input('I\'m sorry, please enter a more API-friendly player name. \n> ')
         player_list.append(new_player)
-    turn_limit = int(input('How many turns before you give up? \n> '))
+    turn_limit = input('How many turns before you give up? \n> ')
+    while True:
+        try:
+            assert(turn_limit.isnumeric())
+            assert(0 < int(turn_limit))
+            break
+        except:
+            turn_limit = input('C\'mon, get your stuff together and give me some good input. \n> ')
     initial_deal_card_num = int(52 / len(player_list))
     extra_cards_to_deal = 52 % len(player_list)
     pile_pile = PilePile(game_deck.deck_id)
@@ -195,25 +290,26 @@ def main():
     print('++++Game successfully initiated++++')
     # continue to take turns until only one player is left
     turn_count = 1
-    while turn_count <= turn_limit:
+    while turn_count <= int(turn_limit):
+        print(f"---------------------------------")
         # clean up piles and remove appropriate players
         players_to_remove = []
         for player in player_list:
             player_draw_pile = pile_pile.get_pile(f'{player}_draw')
-            print(f'Remaining for {player}: {player_draw_pile.remaining}')
+            try:
+                player_win_pile = pile_pile.get_pile(f'{player}_win')
+                win_pile_count = player_win_pile.remaining
+            except:
+                win_pile_count = 0
+            total_cards = player_draw_pile.remaining + win_pile_count
+            print(f'Total cards for {player}: {total_cards}')
             if player_draw_pile.remaining == 0:
-                try:
-                    player_win_pile = pile_pile.get_pile(f'{player}_win')
-                    win_pile_count = player_win_pile.remaining
-                except:
-                    win_pile_count = 0
                 if win_pile_count == 0:
                     print(f"{player} has no cards left! {player} is out of the game.")
                     players_to_remove.append(player)
                 else:
                     pile_pile.pile_to_pile_transfer(f'{player}_win', f'{player}_draw')
                     player_draw_pile.shuffle_pile()
-                    print(f"Cards transferred for {player}")
         if len(players_to_remove) > 0:
             for player in players_to_remove:
                 player_list.remove(player)
@@ -221,8 +317,10 @@ def main():
         if len(player_list) < 2:
             print(f"Congrats {player_list[0]}! You've won the game!")
             break
-        else:  
+        else:
+            print(f"Taking turn...") 
             take_a_turn(player_list, pile_pile)
+
             print(f'Turn completed: {str(turn_count)}')
             turn_count += 1
     else:
